ea73b5e1858058230a6d7a6837e12c436e73bcf6
Christiaan Quyn <christianq010@gmail.com>
2018-02-01 17:47:52 +0530

    Lesson 31: Create a functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Person from './Person/Person';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
+        <Person />
       </div>
     );
   }
