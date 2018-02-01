c75ed43592a0907e8e41a3b4e21696c1efb90a1d
Christiaan Quyn <christianq010@gmail.com>
2018-02-01 18:25:32 +0530

    lesson: 36 - Understanding the children property
    Children are the contents between the <> </> (html opening and closing tags), could be text, ui etc.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <Person name="Chris" age="22"/>
-        <Person name="Hayley" age="42"/>
+        <Person name="Hayley" age="42">Hello this are the children.</Person>
         <Person name="Nuuk" age="21"/>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>I'm a {props.name} who is { props.age } years old</p>
+    return (
+        <div>
+            <p>I'm a {props.name} who is { props.age } years old</p>
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person;
