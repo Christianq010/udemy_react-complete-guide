cb885a3200dec3c65324abe0924f02c6c5b1d456
Christiaan Quyn <christianq010@gmail.com>
2018-02-01 18:05:11 +0530

    Lesson 34: Display dynamic content

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <Person />
+        <Person />
+        <Person />
       </div>
     );
   }
