3d9f6eeb1a8de6d4d8c79bd4b3ab854a74d1c7dd
Christiaan Quyn <christianq010@gmail.com>
2018-02-02 14:49:51 +0530

    Lesson 46: - Styling: Add styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './Person.css';
+
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm a {props.name} who is { props.age } years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
