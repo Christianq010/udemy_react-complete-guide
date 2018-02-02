3a34d83ec7657381d8793be6d9feaf22030c6872
Christiaan Quyn <christianq010@gmail.com>
2018-02-02 14:58:56 +0530

    Lesson 47: - Styling: Add inline styling

--- src/App.js
+++ src/App.js
@@ -34,10 +34,19 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
+        <button style={style} onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.person[0].name} 
                 age={this.state.person[0].age} />
         <Person name={this.state.person[1].name} 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './Person.css';
 
+
 const person = (props) => {
     return (
         <div className="Person">
